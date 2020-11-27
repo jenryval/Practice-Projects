16f2f777f827b87eba3c2a07dd4acdb618fc2310
jenry valdes <jenryval93@gmail.com>
2020-11-27 11:35:09 -0600

    added some JS

---
+++ Expense Tracker/script.js
@@ -0,0 +1,26 @@
+// DOM ELEMENTS
+var expenseName = $(document).get("expenseName")
+var submitBTN = $(document).get("submitBTN")
+var moneyvalue = $(document).get("moneyvalue")
+
+// Submit button event listener 
+expense_list = [];
+TypeOfExpenseBtn.addeventlistener("click", function(){
+    if(!expenseName.value || !moneyvalue.value) return; 
+    let expense = {
+    title: expenseName.value,
+    amount: parseFloat(moneyvalue.value),
+    }
+    expense_list.push(expense);
+    console.log(expense_list)
+})
+
+
+
+//type of expense to show on bottom 
+
+// expense dicription to show next to ^
+
+// same with value
+
+// no refresh when submit it clicked
